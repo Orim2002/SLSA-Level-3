c7fc67c99994b2cf1b230ddad52f02a6cd08820e
orim2002 <o2002mm@gmail.com>
2026-03-07 13:13:14 +0200

    Add ArgoCD ops

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,18 @@
+# Stage 1: Build
+FROM cgr.dev/chainguard/python:latest-dev AS builder
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install --user -r requirements.txt
+
+# Stage 2: Runtime
 FROM cgr.dev/chainguard/python:latest
 WORKDIR /app
+
+# Copy installed packages from the builder stage
+COPY --from=builder /home/nonroot/.local /home/nonroot/.local
 COPY app.py .
-CMD ["app.py"]
+
+# Update PATH to include local bin
+ENV PATH=$PATH:/home/nonroot/.local/bin
+
+ENTRYPOINT ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Hello World, This is Ori Maor sample app")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
+
+@app.route('/')
+def home():
+    return """
+        <h1>Welcome to my Simple Flask App!</h1>
+        <p>Ori Maor SLSA Level 3 sample app.</p>
+        <nav>
+            <a href="/about">About</a>
+        </nav>
+    """
+
+@app.route('/about')
+def about():
+    return """
+        <h1>About This Project</h1>
+        <p>It's simple, but it gets the job done.</p>
+        <a href="/">Go Back Home</a>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
